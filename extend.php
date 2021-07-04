43573dbfa02c5123e75474e1aa40a9e53f7b8df8
Damien Vesper <ldamienvesper@gmail.com>
2021-07-04 18:41:16 -0500

    debug

--- extend.php
+++ extend.php
@@ -27,26 +27,26 @@ return [
     (new Extend\Frontend('admin'))
         ->js(__DIR__ . '/js/dist/admin.js'),
     (new Extend\Frontend('forum'))
-        ->css(__DIR__ . '/less/forum.less')
-        ->content(Stargazing\Listeners\AddAssets::class),
-    (new Extend\Locales(__DIR__ . '/locale')),
-    function (Application $app) {
-        $settings = $app['flarum.settings'];
+        ->css(__DIR__ . '/less/forum.less'),
+        // ->content(Stargazing\Listeners\AddAssets::class),
+    (new Extend\Locales(__DIR__ . '/locale'))//,
+    // function (Application $app) {
+    //     $settings = $app['flarum.settings'];
 
-        $app->resolving('flarum.assets.forum', function (Assets $assets) use ($settings) {
-            $assets->css(function (SourceCollector $sources) use ($settings) {
-                $sources->addString(function () use ($settings) {
-                    $relocateSidebar = (bool)$settings->get('the-turk-stargazing-theme.relocateSidebar', true);
+    //     $app->resolving('flarum.assets.forum', function (Assets $assets) use ($settings) {
+    //         $assets->css(function (SourceCollector $sources) use ($settings) {
+    //             $sources->addString(function () use ($settings) {
+    //                 $relocateSidebar = (bool)$settings->get('the-turk-stargazing-theme.relocateSidebar', true);
 
-                    $vars = [
-                        'config-right-sidebar' => $relocateSidebar ? 'true' : 'false',
-                    ];
+    //                 $vars = [
+    //                     'config-right-sidebar' => $relocateSidebar ? 'true' : 'false',
+    //                 ];
 
-                    return array_reduce(array_keys($vars), function ($string, $name) use ($vars) {
-                        return $string."@$name: {$vars[$name]};";
-                    }, '');
-                });
-            });
-        });
-    }
+    //                 return array_reduce(array_keys($vars), function ($string, $name) use ($vars) {
+    //                     return $string."@$name: {$vars[$name]};";
+    //                 }, '');
+    //             });
+    //         });
+    //     });
+    // }
 ];
