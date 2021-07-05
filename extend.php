535fe22956dcf2bb6acb6deaddbc0d11a5651847
Damien Vesper <ldamienvesper@gmail.com>
2021-07-04 21:45:49 -0500

    Update extend.php

--- extend.php
+++ extend.php
@@ -25,8 +25,8 @@ use Flarum;
 return [
     (new Flarum\Extend\Frontend('forum'))
         ->css(__DIR__.'/less/forum.less')
-        // ->js(__DIR__.'/js/dist/forum.js')
-        ->content(function(Document $document) {
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->content(function($document) {
             $document->body[] = '
             <script type="text/javascript" src="/assets/bubbly-bg.js"></script>
             window.onload = function (e) {
