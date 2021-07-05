c24d89a6622e0a8b892bd3322666964cd062bb98
Damien Vesper <ldamienvesper@gmail.com>
2021-07-04 21:39:41 -0500

    no commas

--- extend.php
+++ extend.php
@@ -25,8 +25,8 @@ use TheTurk\Stargazing;
 
 return [
     (new Extend\Frontend('forum'))
-        ->css(__DIR__.'/less/forum.less'),
-        ->js(__DIR__.'/js/dist/forum.js'),
+        ->css(__DIR__.'/less/forum.less')
+        ->js(__DIR__.'/js/dist/forum.js')
         ->content(function Document $document) {
             $document->body[] = '
             <script type="text/javascript" src="/assets/bubbly-bg.js"></script>
