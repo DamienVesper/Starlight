430911341b5fb04574133560ab18c48d149b0a51
Damien Vesper <ldamienvesper@gmail.com>
2021-07-05 21:11:42 -0500

    dont extend js ples

--- extend.php
+++ extend.php
@@ -24,8 +24,7 @@ use Flarum;
 
 return [
     (new Flarum\Extend\Frontend('forum'))
-        ->css(__DIR__.'/less/forum.less')
-        ->js(__DIR__.'/js/dist/forum.js'),
+        ->css(__DIR__.'/less/forum.less'),
     (new Flarum\Extend\Frontend('admin'))
         ->css(__DIR__.'/less/common/common.less'),
     (new Flarum\Extend\Locales(__DIR__ . '/locale'))
